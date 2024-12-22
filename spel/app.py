609136eb49036a64e5b50bd82689bf0495ceb738
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2024-12-23 00:24:08 +0300

    library implemented

---
+++ spel/app.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import subprocess
+
+def run_file_gather():
+    subprocess.run(["python3", "file_gather.py", "uploads", "py,zip"])
+
+def run_file_share():
+    subprocess.run(["python3", "file_share.py", "deneme"])
+
+if __name__ == "__main__":
+    gather_process = multiprocessing.Process(target=run_file_gather)
+    share_process = multiprocessing.Process(target=run_file_share)
+
+    gather_process.start()
+    share_process.start()
+
+    gather_process.join()
+    share_process.join()
+
+ 
